7ee75dbe117d8caa9552f82155b6a765fc841b33
Mol0D <moloduk99@gmail.com>
2022-12-06 16:42:32 +0200

    initial config for tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,4 @@
   <router-view />
 </template>
 
-<style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: #f6f7fb;
-  height: 100vh;
-}
-</style>
+<style lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vClickOutside from "click-outside-vue3";
+import "@/assets/styles/index.scss";
 
 createApp(App).use(store).use(router).use(vClickOutside).mount("#app");
